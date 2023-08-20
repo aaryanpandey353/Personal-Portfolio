6b4caa2fcb72d7b43d8fcf71deb631f05d7b9a2a
Code With Aaryan <85786184+aaryanpandey353@users.noreply.github.com>
2023-08-20 07:27:54 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer' , 'Android Developer', 'Python Developer', 'Ful-Stack Developer'],
+    typeSpeed: 60,
+  });
+
+var typed = new Typed('#elements', {
+    strings: ['Aaryan Pandey'],
+    typeSpeed: 60,
+  });
